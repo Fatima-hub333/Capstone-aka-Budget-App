8baf363970c29868809696149d3edec4e46de1e5
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-13 21:51:32 +0500

    Create controller for splash screen

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-  get 'records/index'
-  get 'records/show'
-  get 'categories/index'
-  get 'categories/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root "splash#index"
 
   resources :categories, only: [:index, :show] do
     resources :records, only: [:index, :show]
