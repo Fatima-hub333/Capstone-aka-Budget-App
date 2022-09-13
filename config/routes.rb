b1e4fb417198b0bf0a54acdd3ad07ae6e93fb931
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-13 22:16:08 +0500

    Add Devise view

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "splashes#index"
+  # root "home/index"
+  root "home#index"
 
   resources :categories, only: [:index, :show] do
     resources :records, only: [:index, :show]
