4e5348034a39e022b6cefc5ef598de13fc348d39
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2022-09-13 21:43:46 +0500

    Add record controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'records/index'
+  get 'records/show'
   get 'categories/index'
   get 'categories/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
